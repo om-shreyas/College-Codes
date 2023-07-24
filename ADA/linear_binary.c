4f2d63a7aac8a8e6ac58aebd4e645168fa87a1bd
Shreyas Om <shreyas.garain@gmail.com>
2023-07-24 14:31:41 +0530

    Monday July 25

--- ADA/linear_binary.c
+++ ADA/linear_binary.c
@@ -35,17 +35,32 @@ int binary(int N, int A[],int e)
         }
         else if (A[(h+l)/2]>e)
         {
-            printf("low \n");
             h=(h+l)/2;
         }
         else if (A[(h+l)/2]<e)
         {
-            printf("high \n");
             l=(h+l)/2;
         }        
     }
     return(0);
 }
+
+int binary_recursive(int l, int N, int A[],int e)
+{
+    if(A[(N+l)/2]==e)
+    {
+        return((N+l)/2+1);
+    }
+    else if (A[(N+l)/2]>e)
+    {
+        return(binary_recursive(l,(N+l)/2,A,e));
+    }
+    else if (A[(N+l)/2]<e)
+    {
+        return(binary_recursive((N+l)/2,N,A,e));
+    }        
+}
+
 int main()
 {
     int N;
@@ -63,7 +78,8 @@ int main()
     printf("Enter Element for binary search: ");
     scanf("%d",&e);
 
-    printf("Element %d is at %d",e,binary(N,A,e));
+    printf("Element %d is at %d\n",e,binary(N,A,e));
+    printf("Element %d is at %d\n",e,binary_recursive(0,N,A,e));
 
     return(0);
 }
